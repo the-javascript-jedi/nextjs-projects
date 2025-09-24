851cfd16ee1da7da391c496842fbc67634a045c4
nithin.thewebdev <nithin.thewebdev@gmail.com>
2025-09-24 10:00:00 +0530

    moved logic for NewPost

--- max-courses/learn-nextjs/first-app/app/page.js
+++ max-courses/learn-nextjs/first-app/app/page.js
@@ -1,5 +1,6 @@
 import styles from "./page.module.css";
 import Link from "next/link";
+import Header from "../components/header";
 
 export default function Home() {
   console.log("home page running!!");
@@ -7,6 +8,7 @@ export default function Home() {
     <div>
       <main>
         <h1>Hello world</h1>
+        <Header />
         <p>
           <Link href="/about">About Us</Link>
         </p>
---
+++ max-courses/learn-nextjs/first-app/components/header.js
@@ -0,0 +1,8 @@
+function header() {
+  return (
+    <div>
+      <h1>Welcome to NextJS Course</h1>
+    </div>
+  );
+}
+export default header;
