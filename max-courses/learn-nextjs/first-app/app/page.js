14e24bc1c5ca39b1080b84ad6e52482ed7e9504f
nithin.thewebdev <nithin.thewebdev@gmail.com>
2025-09-26 10:00:00 +0530

    added scaffolding

--- max-courses/learn-nextjs/first-app/app/page.js
+++ max-courses/learn-nextjs/first-app/app/page.js
@@ -1,18 +1,9 @@
-import styles from "./page.module.css";
-import Link from "next/link";
-import Header from "../components/header";
-
 export default function Home() {
-  console.log("home page running!!");
   return (
-    <div>
-      <main>
-        <h1>Hello world</h1>
-        <Header />
-        <p>
-          <Link href="/about">About Us</Link>
-        </p>
-      </main>
-    </div>
+    <main>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
+    </main>
   );
 }
