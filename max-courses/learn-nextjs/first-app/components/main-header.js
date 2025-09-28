3069a96d7b11aff7343869be89f37130cd091418
nithin.thewebdev <nithin.thewebdev@gmail.com>
2025-09-28 10:00:00 +0530

    added Image component

--- max-courses/learn-nextjs/first-app/components/main-header.js
+++ max-courses/learn-nextjs/first-app/components/main-header.js
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
+import Image from "next/image";
 
 export default function MainHeader() {
   return (
     <header className={classes.header}>
       <Link className={classes.logo} href="/">
-        <img src={logoImg.src} alt="A plate with food on it" />
+        <Image src={logoImg} alt="A plate with food on it" priority />
         NextLevel Food
       </Link>
       <nav className={classes.nav}>
