a8f5518c10cf386add965cc10c122ed13fe7ecc6
nithin.thewebdev <nithin.thewebdev@gmail.com>
2025-10-05 10:00:00 +0530

    added header

--- max-courses/learn-nextjs/first-app/components/main-header/main-header.js
+++ max-courses/learn-nextjs/first-app/components/main-header/main-header.js
@@ -2,8 +2,10 @@ import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 export default function MainHeader() {
+  const path = usePathname();
   return (
     <header className={classes.header}>
       <Link className={classes.logo} href="/">
@@ -13,7 +15,12 @@ export default function MainHeader() {
       <nav className={classes.nav}>
         <ul>
           <li>
-            <Link href="/meals">Browse Meals</Link>
+            <Link
+              href="/meals"
+              className={path.startsWith("/meals") ? classes.active : undefined}
+            >
+              Browse Meals
+            </Link>
           </li>
           <li>
             <Link href="/community">Foodies Community</Link>
