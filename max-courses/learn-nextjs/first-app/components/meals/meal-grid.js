18712896defed99d3eda5057df9876bd28d506d1
nithin.thewebdev <nithin.thewebdev@gmail.com>
2025-10-09 10:00:00 +0530

    added share page

---
+++ max-courses/learn-nextjs/first-app/components/meals/meal-grid.js
@@ -0,0 +1,16 @@
+import MealItem from "./meal-item";
+import classes from "./meal-grid.module.css";
+
+export default function MealsGrid({ meals }) {
+  return (
+    <>
+      <ul className={classes.meals}>
+        {meals.map((meal) => (
+          <li key={meal.id}>
+            <MealItem {...meal} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
