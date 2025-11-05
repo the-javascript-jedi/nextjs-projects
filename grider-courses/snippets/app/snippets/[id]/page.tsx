90b7b68c2bbdb2133e1b45fdd294e8cb0a0918f1
nithin.thewebdev <nithin.thewebdev@gmail.com>
2025-11-05 09:30:00 +0530

    added css

--- grider-courses/snippets/app/snippets/[id]/page.tsx
+++ grider-courses/snippets/app/snippets/[id]/page.tsx
@@ -10,6 +10,8 @@ interface SnippetShowPageProps {
 export default async function SnippetShowPage(props: any) {
   console.log("props", props);
 
+  // await new Promise((r) => setTimeout(r, 20000));
+
   const { id } = await props.params;
 
   const snippet = await db.snippet.findFirst({
