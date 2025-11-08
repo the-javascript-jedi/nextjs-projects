c69820e10da3e7b27e5ebddb4de60d99b6c7354e
nithin.thewebdev <nithin.thewebdev@gmail.com>
2025-11-08 09:30:00 +0530

    added fetch

--- grider-courses/snippets/app/snippets/[id]/edit/page.tsx
+++ grider-courses/snippets/app/snippets/[id]/edit/page.tsx
@@ -1,3 +1,6 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+
 interface SnippetEditPageProps {
   params: {
     id: string;
@@ -7,7 +10,13 @@ export default async function SnippetEditPage(props: SnippetEditPageProps) {
   console.log("props", props);
   const { id } = await props.params;
   console.log("id", id);
+  const snippet = await db.snippet.findFirst({
+    where: { id: Number(id) },
+  });
 
+  if (!snippet) {
+    return notFound;
+  }
   //   const id = parseInt(props.params.id);
-  return <div>Editing snippet with id {id}</div>;
+  return <div>Editing snippet with title {snippet.title}</div>;
 }
